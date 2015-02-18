804521301fea85477f4ecd7028913937818d4615
bradfordbradford <bradford@wearetelegraph.com>
2015-02-18 15:47:16 -0600

    Select Location Enhanced - pull from array, search via name or address

--- app/js/app.js
+++ app/js/app.js
@@ -51,32 +51,20 @@ $images.imagesLoaded( function() {
 
 // Select 2
 // ---------
-// $(".select-awesome").select2({
-//   placeholder: "Where are you located?"
-// });
+var locations = [
+    { id: 0, text: 'Jefferson Tower <small>2121 W. 12th St. Atlanta, GA</small>' },
+    { id: 1, text: 'Commerce Building <small>128 W. 12th St. Atlanta, GA</small>' },
+    { id: 2, text: 'Fairmont Building <small>128 W. 12th St. Atlanta, GA</small>' },
+    { id: 3, text: 'Park Cities Building <small>100 Broadway St. Atlanta, GA</small>' },
+    { id: 4, text: 'The Joule <small>82 Pacific Ave. Atlanta, GA</small>' },
+    { id: 5, text: 'Belmont Building <small>12 Atlantic Ave. Atlanta, GA</small>' },
+    { id: 6, text: 'Claireview Building <small>2121 Atlantic Ave. Atlanta, GA</small>' },
+    { id: 7, text: 'Transamerica Tower <small>102 Geary St. Atlanta, GA</small>' }
+];
 
-
-// Location Select
-// ---------------
-$('#location-select').magicSuggest({
-    placeholder: 'Enter your delivery location',
-    maxSelection: 1,
-    data: [{
-        name: 'Transamerica Tower',
-        address: '2121 Broadway Ave. Atlanta, GA'
-    },{
-        name: 'Theodore Tower',
-        address: '1708 Atlantic Ave. Atlanta, GA'
-    },{
-        name: 'Jefferson Tower',
-        address: '19 3rd St. Atlanta, GA'
-    },{
-        name: 'Abraham Bldg.',
-        address: 'Alpine Way Atlanta, GA'
-    }],
-    valueField: 'address',
-    renderer: function(data){
-        return data.name + ' <span>' + data.address + '</span>';
-    },
-    resultAsString: true
-});
+$(".location-select").select2({
+  data: locations,
+  placeholder: "Enter your delivery location.",
+  maximumSelectionLength: 1,
+  minimumInputLength: 1,
+})
