1e2e44d5a8a0896fc3c8838af0fce3a2c6e5a5bd
bradfordbradford <bradford@wearetelegraph.com>
2015-02-20 14:33:36 -0600

    Tooltip Menu for Today's Menu + tooltips for Food Meta Tags

--- app/js/app.js
+++ app/js/app.js
@@ -68,3 +68,45 @@ $(".location-select").select2({
   maximumSelectionLength: 1,
   minimumInputLength: 1
 })
+
+
+// Tooltip
+// ---------
+$('[data-food-meta!=""]').qtip({ // Grab all elements with a non-blank data-food-meta attr.
+    content: {
+      attr: 'data-food-meta' // Tell qTip2 to look inside this attr for its content
+    },
+    position: {
+      my: 'top center',
+      at: 'bottom center'
+    },
+    style: {
+      tip: {
+          width: 14,
+          height: 6
+      },
+      classes: 'qtip-food-meta'
+    },
+});
+
+$('[data-menu-by-day!=""]').qtip({ // Grab all elements with a non-blank data-menu-by-day attr.
+    content: {
+      attr: 'data-menu-by-day' // Tell qTip2 to look inside this attr for its content
+    },
+    position: {
+      my: 'top center',
+      at: 'bottom center'
+    },
+    style: {
+      tip: {
+          width: 14,
+          height: 6
+      },
+      classes: 'qtip-menu-by-day'
+    },
+    hide: {
+        fixed: true,
+        delay: 300
+    }
+});
+
