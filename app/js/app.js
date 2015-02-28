0aded47e198d0d466d21c3882374bf9e04dffb91
bradfordbradford <bradford@wearetelegraph.com>
2015-02-27 21:44:05 -0600

    test

--- app/js/app.js
+++ app/js/app.js
@@ -70,45 +70,45 @@ $(".location-select").select2({
 $('.special-select').select2();
 
 
-// // Tooltips
-// // ---------
-// $('[data-food-meta!=""]').qtip({ // Grab all elements with a non-blank data-food-meta attr.
-//     content: {
-//       attr: 'data-food-meta' // Tell qTip2 to look inside this attr for its content
-//     },
-//     position: {
-//       my: 'top center',
-//       at: 'bottom center'
-//     },
-//     style: {
-//       tip: {
-//           width: 14,
-//           height: 6
-//       },
-//       classes: 'qtip-food-meta'
-//     },
-// });
+// Tooltips
+// ---------
+$('[data-food-meta!=""]').qtip({ // Grab all elements with a non-blank data-food-meta attr.
+    content: {
+      attr: 'data-food-meta' // Tell qTip2 to look inside this attr for its content
+    },
+    position: {
+      my: 'top center',
+      at: 'bottom center'
+    },
+    style: {
+      tip: {
+          width: 14,
+          height: 6
+      },
+      classes: 'qtip-food-meta'
+    },
+});
 
-// $('[data-menu-by-day!=""]').qtip({ // Grab all elements with a non-blank data-menu-by-day attr.
-//     content: {
-//       attr: 'data-menu-by-day' // Tell qTip2 to look inside this attr for its content
-//     },
-//     position: {
-//       my: 'top center',
-//       at: 'bottom center'
-//     },
-//     style: {
-//       tip: {
-//           width: 14,
-//           height: 6
-//       },
-//       classes: 'qtip-menu-by-day'
-//     },
+$('[data-menu-by-day!=""]').qtip({ // Grab all elements with a non-blank data-menu-by-day attr.
+    content: {
+      attr: 'data-menu-by-day' // Tell qTip2 to look inside this attr for its content
+    },
+    position: {
+      my: 'top center',
+      at: 'bottom center'
+    },
+    style: {
+      tip: {
+          width: 14,
+          height: 6
+      },
+      classes: 'qtip-menu-by-day'
+    },
 
-//     hide: {
-//         delay: 250,
-//         fixed: true,
-//     }
-// });
+    hide: {
+        delay: 250,
+        fixed: true,
+    }
+});
 
 
