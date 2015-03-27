d15baee33f894eef93fd06f009ddd1b0ebfa8f34
bradfordbradford <bradford@wearetelegraph.com>
2015-03-26 23:55:04 -0500

    Waitlist Form margin correction

--- app/js/app.js
+++ app/js/app.js
@@ -91,9 +91,27 @@ $('[data-menu-by-day!=""]').qtip({ // Grab all elements with a non-blank data-me
 });
 
 
+// SMOOTH
+  // --------------
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
 
 
 // Disabled Link
 // --------------
 $('a.disabled').click(function( event ) {
   event.preventDefault();});
+
