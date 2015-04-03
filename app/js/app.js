7c05dc9affc89a4c77466ca709fe7d28c5de548b
bradfordbradford <bradford@wearetelegraph.com>
2015-04-03 17:20:17 -0500

    Transitions, New Waitlist modal

--- app/js/app.js
+++ app/js/app.js
@@ -1,97 +1,142 @@
-$(document).foundation();
-$(function() {
-    FastClick.attach(document.body);
-});
+console.log('Hide Elements for Effects');
+$('.full-spread-cta').css('opacity: 0');
+
+$(document).ready(function(){
 
 
-// Images - imagesloaded.js
-// ---------------------------------
-$('.fade-img').hide();
-var $images = $('.fade-img');
-$('.fade-img').each(function(){
-  var el = $(this), sources, image;
-  if(sources = el.css('background-image')){
-    $.each(sources.split(','), function(i, source){
-      if(image = source.match(/url\((['"])?(.*?)\1\)/)){
-        $images = $images.add($('<img>').attr('src', image.pop()));
-      }
+  image_loaded = 0;
+  all_images_loaded = 0;
+
+  console.log('Document Ready');
+
+  page_init = function(){
+    setTimeout( function(){
+      $( "body, html" ).scrollTop( 0 );
+      $('#full-spread').waitForImages({
+        finished: function() {
+            all_images_loaded = 1;
+            console.log('Loaded: Full-Spread Image');
+
+            setTimeout( function(){
+            $('.full-spread-cta').addClass('loaded');
+            } , 610 );
+        },
+        waitForAll: true
     });
-  }
-});
+    load_once = 1;
+    console.log('Fade Out: #white-fade');
+    $('#white-fade').addClass('fade-out');
+    setTimeout(function(){$('#white-fade').addClass('hide');}, 410);
 
-$images.imagesLoaded().progress(function(instance, image){
-  var result = image.isLoaded ? 'loaded' : 'broken';
-  console.log('Image:', result, image.img.src);
-});
+    } , 410 );
+  };
 
-$images.imagesLoaded( function() {
-    $('.fade-img').fadeIn('fast');
-});
+  console.log('Home Page!');
+  page_init();
+
+  menu_init = function(){
+    setTimeout(function(){
+      $('.menu-block').waitForImages({
+        finished: function() {
+            all_images_loaded = 1;
+            console.log('Images Load: All loaded');
+
+            setTimeout( function(){
+            $('.menu-block').addClass('loaded');
+            } , 610 );
+        },
+        each: function() {
+           console.log('Images Load: Each loaded');
+        },
+        waitForAll: true
+    });
+    },410);
+  };
+  menu_init();
+  console.log('Menu Page!');
+
+  go_foundation = function(){
+    // $(function() {
+    //   FastClick.attach(document.body);
+    // });
+    $(document).foundation();
+    // Modal
+    $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+      $('body').addClass('no-overflow');
+      var modal = $(this);
+      setTimeout(function(){
+        modal.addClass('opened');
+      }, 500);
+    });
+    $(document).on('close.fndtn.reveal', '[data-reveal]', function () {
+      $('body').removeClass('no-overflow');
+      var modal = $(this);
+      modal.removeClass('opened');
+    });
+  };
+
+  console.log('Go Foundation!');
+  go_foundation();
 
 
-
-// Modal
-$(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
-  $('body').addClass('no-overflow');
-  var modal = $(this);
-  setTimeout(function(){
-    modal.addClass('opened');
-  }, 500);
-});
-$(document).on('close.fndtn.reveal', '[data-reveal]', function () {
-  $('body').removeClass('no-overflow');
-  var modal = $(this);
-  modal.removeClass('opened');
-});
+  // In View
+  (function($) {
+    $(document).ready(function() {
+      $(window).scroll(function() {
+        $('.viewTest:in-viewport(400)').addClass('in-view');
+      });
+    });
+  }(jQuery));
 
 
-// Select 2
-// ---------
-$('.special-select').select2();
+  // Select 2
+  // ---------
+  $('.special-select').select2();
 
-// Tooltips
-// ---------
-$('[data-food-meta!=""]').qtip({ // Grab all elements with a non-blank data-food-meta attr.
-    content: {
-      attr: 'data-food-meta' // Tell qTip2 to look inside this attr for its content
-    },
-    position: {
-      my: 'top center',
-      at: 'bottom center'
-    },
-    style: {
-      tip: {
-          width: 14,
-          height: 6
+
+  // Tooltips
+  // ---------
+  $('[data-food-meta!=""]').qtip({ // Grab all elements with a non-blank data-food-meta attr.
+      content: {
+        attr: 'data-food-meta' // Tell qTip2 to look inside this attr for its content
       },
-      classes: 'qtip-food-meta'
-    },
-});
-
-$('[data-menu-by-day!=""]').qtip({ // Grab all elements with a non-blank data-menu-by-day attr.
-    content: {
-      attr: 'data-menu-by-day' // Tell qTip2 to look inside this attr for its content
-    },
-    position: {
-      my: 'top center',
-      at: 'bottom center'
-    },
-    style: {
-      tip: {
-          width: 14,
-          height: 6
+      position: {
+        my: 'top center',
+        at: 'bottom center'
       },
-      classes: 'qtip-menu-by-day'
-    },
+      style: {
+        tip: {
+            width: 14,
+            height: 6
+        },
+        classes: 'qtip-food-meta'
+      },
+  });
 
-    hide: {
-        delay: 250,
-        fixed: true,
-    }
-});
+  $('[data-menu-by-day!=""]').qtip({ // Grab all elements with a non-blank data-menu-by-day attr.
+      content: {
+        attr: 'data-menu-by-day' // Tell qTip2 to look inside this attr for its content
+      },
+      position: {
+        my: 'top center',
+        at: 'bottom center'
+      },
+      style: {
+        tip: {
+            width: 14,
+            height: 6
+        },
+        classes: 'qtip-menu-by-day'
+      },
+
+      hide: {
+          delay: 250,
+          fixed: true,
+      }
+  });
 
 
-// SMOOTH
+  // SMOOTH
   // --------------
   $(function() {
     $('a[href*=#]:not([href=#]):not(".no-scroll")').click(function() {
@@ -108,33 +153,77 @@ $('[data-menu-by-day!=""]').qtip({ // Grab all elements with a non-blank data-me
     });
   });
 
+  // Hover - Slide Up
+  $('.hover-slideUp').hover(function(){
+    $(this).stop().animate({ top: -4}, 'slow', 'easeInOutCirc');
+    }, function() {
+        $(this).stop().animate({ top: 0}, 'slow', 'easeInOutCirc');
+  });
 
 
-// Disabled Link
-// --------------
-$('a.disabled').click(function( event ) {
-  event.preventDefault();});
+  // Disabled Link
+  // --------------
+  $('a.disabled').click(function( event ) {
+    event.preventDefault();});
 
 
-// Cart Update Alert
-// -----------------
-$('.menu-detail .order a').click(function(event) {
-  event.preventDefault();
-  $('.cart-update-alert').fadeIn('slow').slideUp('slow', 'easeInOutCirc');
+  // Cart Update Alert
+  // -----------------
+  $('.menu-detail .order a').click(function(event) {
+    event.preventDefault();
+    $('.cart-update-alert').fadeIn('slow').slideUp('slow', 'easeInOutCirc');
+  });
+
+
+  // Partner Card Hover
+  // -------------------
+  $('.partner-card').hover(function() {
+    var findContentHeight = $(this).find('.content')
+    var height = $(findContentHeight).height()
+
+    $(this).find('.content-wrap').stop().animate({ top: -height, opacity: .98 }, 'slow', 'easeInOutCirc');
+    }, function() {
+        $(this).find('.content-wrap').stop().animate({ top: 0,  opacity: 1 }, 'slow', 'easeInOutCirc');
+  });
+
+
 });
 
 
-// Partner Card Hover
-// -------------------
 
 
-$('.partner-card').hover(function() {
-  var findContentHeight = $(this).find('.content')
-  var height = $(findContentHeight).height()
 
-  $(this).find('.content-wrap').stop().animate({ top: -height, opacity: .98 }, 'slow', 'easeInOutCirc');
-  }, function() {
-      $(this).find('.content-wrap').stop().animate({ top: 0,  opacity: 1 }, 'slow', 'easeInOutCirc');
-});
+
+
+
+// Images - imagesloaded.js
+// ---------------------------------
+// $('.fade-img').hide();
+// var $images = $('.fade-img');
+// $('.fade-img').each(function(){
+//   var el = $(this), sources, image;
+//   if(sources = el.css('background-image')){
+//     $.each(sources.split(','), function(i, source){
+//       if(image = source.match(/url\((['"])?(.*?)\1\)/)){
+//         $images = $images.add($('<img>').attr('src', image.pop()));
+//       }
+//     });
+//   }
+// });
+
+// $images.imagesLoaded().progress(function(instance, image){
+//   var result = image.isLoaded ? 'loaded' : 'broken';
+//   console.log('Image:', result, image.img.src);
+// });
+
+// $images.imagesLoaded( function() {
+//     $('.fade-img').fadeIn('fast');
+// });
+
+
+
+
+
+
 
 
